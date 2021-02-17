ecc11aaff942971af6531e47f92ffa8b0ceb5ccb
Pratik Senjaliya <pratik@solutelabs.com>
2021-02-17 11:41:36 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ class App extends Component{
       {id:'3',name:'MacBook',price:'250000'},
       {id:'4',name:'One Plus',price:'35000'}
     ],
-    emptystate:"Your cart is empty"
+
   }
 
   deleteItemHandler=(item_index)=>{
     const Item_arr1=[...this.state.items];
     Item_arr1.splice(item_index,1);
-    this.setState({items:Item_arr1});
+    this.setState({items:Item_arr1},()=>{console.log("State is Updated.")});
   }
 
   
@@ -23,25 +23,38 @@ class App extends Component{
   render(){
 
     let items_arr=null;
+    const Item_arr2=[...this.state.items];
     if(this.state.items){
       items_arr=(
         <div>
           {
             this.state.items.map((item,index)=>{
-              return <Items name={item.name} price={item.price} click={()=>this.deleteItemHandler(index)}/> 
+              return <Items name={item.name} price={item.price} click={()=>this.deleteItemHandler(index)} key={item.id}/> 
             })
           }
         </div>
       )
     }
-
-    return (
-    <div className="App">
-      <h1>SHOPING CART</h1>
-      <div className="Category">Mobile Phones</div>
-      {items_arr}
-    </div>
-  );
+    if(Item_arr2.length==0){
+      return (
+        <div className="App">
+          <h1>SHOPING CART</h1>
+          <div className="Category">Mobile Phones</div>
+          <p>Your Cart is empty!</p>
+        </div>
+      );
+    }
+    else{
+      return(
+        <div className="App">
+          <h1>SHOPING CART</h1>
+          <div className="Category">Mobile Phones</div>
+          {items_arr}
+        </div>
+      );
+    }
+      
+    
   } 
 }
 
